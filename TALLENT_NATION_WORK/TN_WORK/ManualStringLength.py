7b87f5aaf4d6470abf5f464054c1d8cc25fb5568
idakpan <Oladosuolamide330@gmail.com>
2026-08-07 16:37:10 +0100

    ManualStringLength

---
+++ TALLENT_NATION_WORK/TN_WORK/ManualStringLength.py
@@ -0,0 +1,8 @@
+# Implement str_len(value) without using len(). Count the characters manually 
+# and return the number of characters in the string.
+
+def str_len(value):
+    val1 = 0
+    for val2 in value:
+        val1 = val1 +1
+    return val1
