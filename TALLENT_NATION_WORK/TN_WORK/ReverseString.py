156757a650f21b0e220d7f8dc4a0a463d090242b
idakpan <Oladosuolamide330@gmail.com>
2026-08-10 17:20:10 +0100

    rivers 2_using slice

--- TALLENT_NATION_WORK/TN_WORK/ReverseString.py
+++ TALLENT_NATION_WORK/TN_WORK/ReverseString.py
@@ -1,7 +1,11 @@
 def reverse_string(value):
-    reversed_str = ""
-    
-    for char in value:
+   reversed_str = ""
+   for char in value:
        reversed_str = char + reversed_str
-        
-    return reversed_str
+   return reversed_str
+# print(reverse_string("ba"))
+
+listsn = [1,2,3,4,"a","b","c","d"]
+nwln = listsn[1:2]
+
+print(len(nwln))
